42146f0f993d2b9f3ca18b3886109b344fa237e1
Simon Elvery <simon@elvery.net>
2012-11-29 09:27:21 +1000

    Class documentation.

--- code/CronLog.php
+++ code/CronLog.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Extend silverstripe logging to give cron a log separate to the general SS log.
+ * 
+ * @package pseudocron
+ */
 class CronLog extends SS_Log {
 	protected static $logger;
 	const INFO = Zend_Log::INFO;
